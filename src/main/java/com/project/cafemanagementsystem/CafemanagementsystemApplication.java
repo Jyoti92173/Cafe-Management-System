16d6cdf0a58bbc6f70b6d5abcbd2c91f1d8fe605
Jyoti Kumari <jk648892@gmail.com>
2024-04-09 18:47:16 +0530

    Initial commit

---
+++ src/main/java/com/project/cafemanagementsystem/CafemanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.cafemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafemanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafemanagementsystemApplication.class, args);
+	}
+
+}
